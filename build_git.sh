5709a95cddb00136f46307a4bd5a03a9cfa36108
Matthias Krüger <matthias.krueger@famsik.de>
2017-01-15 02:53:38 +0100

    stage 1: rename path variables

--- build_git.sh
+++ build_git.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
-root_dir=`pwd`
+
+# This script bootstraps llvm, clang and friends in optimized way
+# requires gold linker (for lto)
+
+
+procs=5 #number of jobs to run at a time
+export CCACHE_DISABLE=1 #disable ccache
+rootDir=`pwd` #cwd
 
 mkdir -p  stage_1
 cd stage_1
 
+export stageBase=`pwd`
+export LLVMSrc=${stageBase}/llvm  #llvm
+export clangSrc=${LLVMSrc}/tools/clang  #clang
+export toolsExtraSrc=${LLVMSrc}/tools/clang/tools/extra #tools
+export compilerRTSrc=${LLVMSrc}/projects/compiler-rt #sanitizers
+export pollySrc=${LLVMSrc}/tools/polly #polly
+export lldSRC=${BASE}/llvm/tools/lld #lld linker
 
-#if [ -z $procs ] ; then
-	procs=5
-#fi
-
-export BASE=`pwd`
-export LLVM_SRC=${BASE}/llvm
-export CLANG_SRC=${LLVM_SRC}/tools/clang
-export TOOLS_EXTRA_SRC=${LLVM_SRC}/tools/clang/tools/extra
-export COMPILERRT_SRC=${BASE}/llvm/projects/compiler-rt
-export POLLY_SRC=${BASE}/llvm/tools/polly
-#export LIBCXX_SRC=${BASE}/llvm/tools/libcxx #cxx libs
-#export LIBCXXABI_SRC=${BASE}/llvm/projects/libc++abi
-export LLD_SRC=${BASE}/llvm/tools/lld
-
-export LLVM_BUILD=${BASE}/build
+# build dir
+export LLVMBuild=${stageBase}/build
 
 export CXX=clang++
 export CC=clang
 
+mkdir -p ${LLVMBuild}
+cd ${LLVMBuild}
 
-export CCACHE_DISABLE=1
-
-mkdir -p ${LLVM_BUILD}
-cd ${LLVM_BUILD}
 cmake ../llvm -G "Unix Makefiles" \
 	-DDISABLE_ASSERTIONS=1 \
 	-DENABLE_OPTIMIZED=1 \
