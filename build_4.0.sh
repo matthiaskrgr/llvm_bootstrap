8fe4480979e179c4e8b0f78d8367e90949b0cbcf
Matthias Krüger <matthias.krueger@famsik.de>
2017-02-04 13:42:40 +0100

    add WIP 4.0 buildscript

---
+++ build_4.0.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+VERSION="4.0.0rc1"
+
+#get archives
+wget "http://llvm.org/pre-releases/4.0.0/rc1/llvm-${VERSION}.src.tar.xz"
+wget "http://llvm.org/pre-releases/4.0.0/rc1/cfe-${VERSION}.src.tar.xz"
+wget "http://llvm.org/pre-releases/4.0.0/rc1/compiler-rt-${VERSION}.src.tar.xz"
+wget "http://llvm.org/pre-releases/4.0.0/rc1/lld-${VERSION}.src.tar.xz"
+wget "http://llvm.org/pre-releases/4.0.0/rc1/polly-${VERSION}.src.tar.xz"
+wget "http://llvm.org/pre-releases/4.0.0/rc1/clang-tools-extra-${VERSION}.src.tar.xz"
+
+#get signatures
+
+wget "http://llvm.org/pre-releases/4.0.0/rc1/llvm-${VERSION}.src.tar.xz.sig"
+wget "http://llvm.org/pre-releases/4.0.0/rc1/cfe-${VERSION}.src.tar.xz.sig"
+wget "http://llvm.org/pre-releases/4.0.0/rc1/compiler-rt-${VERSION}.src.tar.xz.sig"
+wget "http://llvm.org/pre-releases/4.0.0/rc1/lld-${VERSION}.src.tar.xz.sig"
+wget "http://llvm.org/pre-releases/4.0.0/rc1/polly-${VERSION}.src.tar.xz.sig"
+wget "http://llvm.org/pre-releases/4.0.0/rc1/clang-tools-extra-${VERSION}.src.tar.xz.sig"
+
+
+# This script bootstraps llvm, clang and friends in optimized way
+# requires gold linker (for lto) http://llvm.org/docs/GoldPlugin.html 
+# and ninja https://ninja-build.org/
+
+procs=5 #number of jobs to run at a time
+export CCACHE_DISABLE=1 #disable ccache
+rootDir=`pwd` #cwd
+
+mkdir -p  stage_1
+cd stage_1
+
+export stageBase=`pwd`
+export LLVMSrc=${stageBase}/llvm  #llvm
+export clangSrc=${LLVMSrc}/tools/clang  #clang
+export toolsExtraSrc=${LLVMSrc}/tools/clang/tools/extra #tools
+export compilerRTSrc=${LLVMSrc}/projects/compiler-rt #sanitizers
+export pollySrc=${LLVMSrc}/tools/polly #polly
+export lldSRC=${stageBase}/llvm/tools/lld #lld linker
+
+# build dir
+export LLVMBuild=${stageBase}/build
+#compilers to use for stage 1
+export CXX=clang++
+export CC=clang
+
+
+echo "Cloning/updating repos..."
+
+
+echo llvm
+if ! test -d ${LLVMSrc}; then
+	git clone http://llvm.org/git/llvm.git ${LLVMSrc}
+else
+	cd ${LLVMSrc}
+	git pull
+fi
+
+echo clang
+if ! test -d ${clangSrc}; then
+	git clone http://llvm.org/git/clang.git ${clangSrc}
+else
+	cd ${clangSrc}
+	git pull
+fi
+
+echo clang-tools-extra
+if ! test -d ${toolsExtraSrc}; then
+	git clone http://llvm.org/git/clang-tools-extra.git ${toolsExtraSrc}
+else
+	cd ${toolsExtraSrc}
+	git pull
+fi
+
+echo compiler-rt
+if ! test -d ${compilerRTSrc}; then
+    git clone http://llvm.org/git/compiler-rt.git ${compilerRTSrc}
+else
+	cd ${compilerRTSrc}
+	git pull
+fi
+
+echo polly
+if ! test -d ${pollySrc}; then
+	git clone http://llvm.org/git/polly.git ${pollySrc}
+else
+	cd ${pollySrc}
+	git pull
+fi
+
+echo lld
+if ! test -d ${lldSRC}; then
+	git clone http://llvm.org/git/lld.git ${lldSRC}
+else
+	cd ${lldSRC}
+	git pull
+fi
+
+# start building
+
+mkdir -p ${LLVMBuild}
+cd ${LLVMBuild}
+
+cmake ../llvm -G "Ninja" \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DLLVM_BINUTILS_INCDIR=/usr/include \
+	-DCMAKE_C_FLAGS="-march=native -O3 -g0 -DNDEBUG" \
+	-DCMAKE_CXX_FLAGS="-march=native -O3 -g0 -DNDEBUG" \
+	-DLLVM_PARALLEL_LINK_JOBS=1 \
+	-DLLVM_TARGETS_TO_BUILD="X86" \
+	-DLLVM_OPTIMIZED_TABLEGEN=1 \
+	-DLLVM_BUILD_TOOLS=0 
+
+nice -n 15 ninja-build -l $procs -j $procs clang LLVMgold llvm-ar llvm-ranlib lld || exit
+echo "stage 1 done"
+
+# clang compiled with system clang is done
+# now, compile clang again with the newly built version
+cd ${rootDir}
+
+export cloneRoot=${rootDir}/stage_1/
+mkdir -p stage_2
+cd stage_2
+export stageBase=`pwd`
+export LLVMSrc=${stageBase}/llvm
+export clangSrc=${LLVMSrc}/tools/clang
+export toolsExtraSrc=${LLVMSrc}/tools/clang/tools/extra
+export compilerRTSrc=${stageBase}/llvm/projects/compiler-rt
+export pollySrc=${stageBase}/llvm/tools/polly
+export lldSRC=${stageBase}/llvm/tools/lld
+
+export LLVMBuild=${stageBase}/build
+
+
+# we can simply clone from local to local repo, no need to pull everything from the net again
+
+echo llvm
+if ! test -d ${LLVMSrc}; then
+    git clone ${cloneRoot}/llvm ${LLVMSrc}
+else
+	cd ${LLVMSrc}
+	git pull
+fi
+
+echo clang
+if ! test -d ${clangSrc}; then
+	git clone ${cloneRoot}/llvm/tools/clang ${clangSrc}
+else
+	cd ${clangSrc}
+	git pull
+fi
+
+echo tools
+if ! test -d ${toolsExtraSrc}; then
+	git clone ${cloneRoot}/llvm/tools/clang/tools/extra ${toolsExtraSrc}
+else
+	cd ${toolsExtraSrc}
+	git pull
+fi
+
+echo copiler-rt
+if ! test -d ${compilerRTSrc}; then
+	git clone  ${cloneRoot}/llvm/projects/compiler-rt ${compilerRTSrc}
+else
+	cd ${compilerRTSrc}
+	git pull
+fi
+
+echo polly
+if ! test -d ${pollySrc}; then
+	git clone  ${cloneRoot}/llvm/tools/polly ${pollySrc}
+else
+	cd ${pollySrc}
+	git pull
+fi
+
+echo lld
+if ! test -d ${lldSRC}; then
+	git clone  ${cloneRoot}/llvm/tools/lld ${lldSRC}
+else
+	cd ${lldSRC}
+	git pull
+fi
+
+
+
+# use new clang++
+export CXX="${rootDir}/stage_1/build/bin/clang++"
+export CC="${rootDir}/stage_1/build/bin/clang"
+mkdir -p ${LLVMBuild}
+cd ${LLVMBuild}
+
+
+cmake ../llvm -G "Ninja" \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DLLVM_BINUTILS_INCDIR=/usr/include \
+	-DCMAKE_C_FLAGS="-march=native -O3  -g0 -DNDEBUG" \
+	-DCMAKE_CXX_FLAGS="-march=native -O3  -g0 -DNDEBUG" \
+	-DLLVM_PARALLEL_LINK_JOBS=2 \
+	-DLLVM_OPTIMIZED_TABLEGEN=1 \
+	-DLLVM_TARGETS_TO_BUILD="X86" \
+	-DLLVM_ENABLE_LTO="Full" \
+	-DCMAKE_AR="${rootDir}/stage_1/build/bin/llvm-ar" \
+	-DCMAKE_RANLIB="${rootDir}/stage_1/build/bin/llvm-ranlib" \
+	-DLLVM_USE_LINKER="${rootDir}/stage_1/build/bin/ld.lld" 
+
+
+
+nice -n 15 ninja-build -l $procs -j $procs clang LLVMgold asan ubsan scan-build llvm-objdump llvm-opt-report compiler-rt lld llvm-ar llvm-ranlib bugpoint || exit
+#nice -n 15 ninja-build -l $procs -j $procs check-all  # building this will take ages with lto, also take care having automatic core dumps disabled before running
+echo "stage 2 done"
