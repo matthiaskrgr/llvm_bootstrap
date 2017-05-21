8d495b89e77fec1d7e565a6dd6fb9d3c86baf849
Matthias Krüger <matthias.krueger@famsik.de>
2017-05-21 02:51:35 +0200

    buildbot status: handle builds being in progress

--- BB_status.py
+++ BB_status.py
@@ -2,13 +2,18 @@
 import json
 import requests
 
-# checks status of last 3 builds of llvm clang-with-lto-ubuntu buildbot slave
+# checks status of last n builds of llvm clang-with-lto-ubuntu buildbot slave
 
 url_Bslave="http://lab.llvm.org:8011/json/builders/clang-with-lto-ubuntu"
 r = requests.get(url_Bslave)
 j = r.json()
-for buildNr in reversed(j['cachedBuilds'][-3:]): # last 3 buids
+for buildNr in reversed(j['cachedBuilds'][-5:]): # last 5 buids
 	url_Bbuild = "http://lab.llvm.org:8011/json/builders/clang-with-lto-ubuntu/builds/" + str(buildNr)
 	r_Bbuild = requests.get(url_Bbuild)
 	j_Bbuild = r_Bbuild.json()
-	print(str(buildNr) + ": " + j_Bbuild['text'][0] + "\t" + j_Bbuild['text'][1])
+	txt = j_Bbuild['text']
+	if (txt):
+		print(str(buildNr) + ": " + txt[0] + "\t" + txt[1])
+	else:  # build still in progress
+		print(str(buildNr) + ": in progress...")
+
