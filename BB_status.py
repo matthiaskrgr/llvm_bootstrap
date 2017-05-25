dc8c2fabcf2eb1fa3ab389622715766064659bc2
Matthias Krüger <matthias.krueger@famsik.de>
2017-05-25 15:57:43 +0200

    BB_status: print times

--- BB_status.py
+++ BB_status.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import json
 import requests
+import datetime  # unix to HR time
 
 # checks status of last n builds of llvm clang-with-lto-ubuntu buildbot slave
 
@@ -12,8 +13,16 @@ for buildNr in reversed(j['cachedBuilds'][-5:]): # last 5 buids
 	r_Bbuild = requests.get(url_Bbuild)
 	j_Bbuild = r_Bbuild.json()
 	txt = j_Bbuild['text']
-	if (txt):
-		print(str(buildNr) + ": " + txt[0] + "\t" + txt[1])
-	else:  # build still in progress
-		print(str(buildNr) + ": in progress...")
+	# handle times
+	times = j_Bbuild['times']
+	# [starttime,endtime], for running jobs [starttime, "None"]
+	start_time = datetime.datetime.fromtimestamp(float(times[0])).strftime('%H:%M:%S')
+	if str(times[1]) == "None":
+		end_time = "now"
+	else:
+		end_time = datetime.datetime.fromtimestamp(float(times[1])).strftime('%H:%M:%S')
 
+	if (txt):
+		print(str(buildNr) + ": " + txt[0] + "\t" + txt[1] + " " + start_time + " - " + end_time)
+	else:  # build still in progress
+		print(str(buildNr) + " " + start_time + " - " + end_time)
